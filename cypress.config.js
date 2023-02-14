de01ec5324ed0c669d12612efce7e290a9c35488
zenitfan88 <nikanenkov@rambler.ru>
2023-02-14 20:23:13 +0400

    add parallel

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,6 @@ module.exports = defineConfig({
   projectId: "u57h2u",
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
     "retries": 1,
     "baseUrl": "https://qamid.tmweb.ru/client/index.php",
