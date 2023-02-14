8f3048fc798a61d3e33da2e5185012ec5ad3372d
zenitfan88 <nikanenkov@rambler.ru>
2023-02-14 19:00:42 +0400

    add parallel

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "3jv5ns",
+  projectId: "u57h2u",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
